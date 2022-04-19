84659638437a260961d2736cd17cc7cef833aca0
Tim Lange <Tim.lange@park-now.com>
2022-04-19 23:11:32 +0200

    Day 8
    - Files (read/write)
    - using
    - this

---
+++ Files/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @""; //path
+            string text = "Some info";
+
+            System.IO.File.AppendAllText(path, text);
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ StreamWriter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @""; //path
+            string[] lines = { "Hello 1", "Hello 2", "Hello 3" };
+
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(path))
+            {
+                foreach(var line in lines)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+
+        }
+    }
+}
