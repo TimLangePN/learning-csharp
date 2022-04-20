4ca90c8bf8e9d52ef3b6f132b8acd412c3d76e23
Tim Lange <Tim.lange@park-now.com>
2022-04-20 22:30:32 +0200

    Day 9
    - Throw keyword
    - Try Catch Finally

---
+++ ThrowKeyword/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int ourMoney = OurMoney(10, -10);
+            EveryonesMoney();
+        }
+
+        public static int EveryonesMoney()
+        {
+            int everyonesMoney = 0;
+
+            try
+            {
+                everyonesMoney = OurMoney(10, 10) + TheirMoney(10 ,-10);
+            }
+            catch(Exception ex)
+            {
+                //log in db etc
+                throw new Exception("Some extra info", ex);
+            }
+
+            return everyonesMoney;
+        }
+
+        public static int TheirMoney(int hisMoney, int herMoney)
+        {
+            if (hisMoney < 0 || herMoney < 0) throw new ArithmeticException($"her money or his money is not valid");
+            return hisMoney + herMoney;
+        }
+
+        public static int OurMoney(int myMoney, int yourMoney)
+        {
+            if (myMoney < 0 || yourMoney < 0) throw new ArithmeticException($"my money or your money is not valid");
+            return myMoney + yourMoney;
+        }
+    }
+}
---
+++ TryCatchFinally/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @""; //path
+            string[] lines = { "Hello 1", "Hello 2", "Hello 3" };
+
+            //using (System.IO.StreamWriter writer = new System.IO.StreamWriter(path))
+            //{
+            //    foreach (var line in lines)
+            //    {
+            //        writer.WriteLine(line);
+            //    }
+            //}
+
+            StreamWriter writer = null;
+            try
+            {
+                writer = new System.IO.StreamWriter(path);
+                foreach (var line in lines)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (writer != null)
+                {
+                    writer.Dispose();
+                }
+            }
+        }
+    }
+}
